e52e81c5c317d4b9c0bda1c5a85db1a875ebf61c
Eber Rodrigues <eberjoe@gmail.com>
2019-10-04 21:54:33 -0300

    Create ClientApp.java

---
+++ client/ClientApp.java
@@ -0,0 +1,17 @@
+package clientapp;
+
+import java.net.Socket;
+import java.io.*;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+        try{
+        Socket client = new Socket("192.168.6.78", 8830);
+        System.out.println("Conectado! " + client.toString());
+        } catch (IOException ex) {
+            System.out.println("Erro criando o cliente!");
+        }
+    }
+    
+}
